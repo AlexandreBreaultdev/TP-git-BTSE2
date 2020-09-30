d93b55ba1cbcf7738e302cfc576b4b56eda90e67
Alexandre Breault <a.breault@iia-laval>
2020-09-30 11:14:52 +0200

    Delete file merge

--- test/test_app_LOCAL_518.bat
+++
@@ -1,4 +0,0 @@
-
-### test_app.bat - 2020 - BREAULT ###
-version = "1.2.1"
-echo "version ${version}"
--- test/test_app_REMOTE_518.bat
+++
@@ -1,4 +0,0 @@
-### test_app.bat - 2020 - BREAULT ###
-version = "1.2.0-b"
-
-echo "version ${version}"
